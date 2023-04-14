8e428c79f81b9f76bc4dddb554d1c39660552073
Leo Almeida <leowoa@outlook.com>
2023-04-14 14:19:21 -0300

    created the logic to generate password

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const output = document.getElementById("demo");
 output.innerHTML = slider.value;
 const inputResult = document.getElementById("myInput")
 
-let length;
+var length = 10;
 
 slider.oninput = function() {
     output.innerHTML = this.value;
@@ -15,25 +15,28 @@ const upper = document.getElementById("upper");
 const number = document.getElementById("number");
 const noRepeat = document.getElementById("similar");
 
-let specialState;
-let upperState;
-let numberState;
-let noRepeatState;
+let specialState = [0];
+let upperState = [0];
+let numberState = [0];
+let noRepeatState = [0];
 
 checkState(special, specialState);
 checkState(upper, upperState);
 checkState(number, numberState);
 checkState(noRepeat, noRepeatState);
 
-function checkState(toggle, state){
+function checkState(toggle, state) {
     toggle.addEventListener("change", function (event) {
         if (event.target.checked){
-            state = true;
+            state.pop();
+            state.push(1);
         } else {
-            state = false
+            state.pop();
+            state.push(0);
         }
     });
 }
+
 function copyFunction() {
     const copyText = document.getElementById("myInput");
 
@@ -43,45 +46,32 @@ function copyFunction() {
     navigator.clipboard.writeText(copyText.value);
 }
 
-
-
-
-
 const getRandomElement = arr => {
     const rand = Math.floor(Math.random() * arr.length);
     return arr[rand];
 }
 
-
-const generateRandomPasswordSelection = (length) => {
+const genPass = (lengthLocal) => {
     const uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
     const lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     const special = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '=', '{', '}', '[', ']', ':', ';', '?', ', ', '.', '|', '\\'];
     const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
     const nonSpecial = [...uppercase, ...lowercase, ...numbers];
-    let password = '';
+    var password = '';
 
-    if (numberState && !specialState && !upperState && !noRepeatState) { //only number true
-        for (let i = 0; i < length; i++) {
-            password += getRandomElement([...lowercase, ...uppercase])
+    if (numberState[0] === 1 && specialState[0] === 0 && upperState[0] === 0 && noRepeatState[0] === 0) { //only number true
+        for (let i = 0; i < lengthLocal; i++) {
+            password += getRandomElement([...lowercase, ...numbers]);
         }
-    } else {
-        for (let i = 0; i < length; i++) { //all true;
-            // Previous character is a special character
-            if (i !== 0 && special.includes(password[i - 1])) {
-                password += getRandomElement(nonSpecial);
-            } else password += getRandomElement([...nonSpecial, ...special]);
-        }
+    } else if(numberState[0] === 1 && specialState[0] === 0 && upperState[0] === 0 && noRepeatState[0] === 0){
+
     }
-
-
     return password;
-}
+};
 
-
-function buttonClicked(){
-    var pass = generateRandomPasswordSelection(length)
+function buttonClicked() {
+    const pass = genPass(length);
     console.log(pass)
-    inputResult.innerHTML = pass;
+    inputResult.value = pass;
 }
